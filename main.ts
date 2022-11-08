1b1e6dd5f1091281ef5753a2f18050c88dcd2a22
melliodas7717 <75526038+melliodas7717@users.noreply.github.com>
2022-11-08 10:25:03 +0900

    main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -4,7 +4,6 @@ input.onButtonPressed(Button.A, function () {
 input.onButtonPressed(Button.B, function () {
     ch.change(LedSpriteProperty.X, 1)
 })
-let Combo = 0
 let ch: game.LedSprite = null
 basic.showString("3")
 basic.showString("2")
@@ -14,34 +13,18 @@ let _ = game.createSprite(randint(0, 4), 0)
 game.setLife(5)
 basic.forever(function () {
     for (let index = 0; index < 4; index++) {
-        _.change(LedSpriteProperty.Y, Combo * 2)
-        basic.pause(250 - 0)
+        _.change(LedSpriteProperty.Y, 1)
+        basic.pause(200)
     }
     if (_.isTouching(ch)) {
-        if (Combo == 20) {
-            game.addScore(3)
-            basic.pause(100)
-            _.delete()
-            _ = game.createSprite(randint(0, 4), 0)
-        } else {
-            if (Combo >= 10) {
-                game.addScore(2)
-                basic.pause(100)
-                _.delete()
-                _ = game.createSprite(randint(0, 4), 0)
-            } else {
-                game.addScore(1)
-                basic.pause(100)
-                _.delete()
-                _ = game.createSprite(randint(0, 4), 0)
-            }
-            Combo += 1
-        }
+        game.addScore(1)
+        _.delete()
+        _ = game.createSprite(randint(0, 4), 0)
+        basic.pause(200)
     } else {
         game.removeLife(1)
         _.delete()
         _ = game.createSprite(randint(0, 4), 0)
-        Combo = 0
         basic.pause(1000)
     }
 })
